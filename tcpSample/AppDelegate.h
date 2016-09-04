2f9ad3053145f8f7909e994faf0e0b48e8c5b18a
Peng Li <seudut@gmail.com>
2016-09-05 00:12:52 +0800

    initial xcode project commit

---
+++ tcpSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tcpSample
+//
+//  Created by Peng Li on 9/5/16.
+//  Copyright © 2016 Peng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tcpSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tcpSample
+//
+//  Created by Peng Li on 9/5/16.
+//  Copyright © 2016 Peng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
